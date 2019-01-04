bfa5a9e6772b5c4049b20a65c7b468f4d3adcdbe
mkirchner <mail@marc-kirchner.de>
2019-01-03 23:17:10 -0800

    Features/docs (#3)
    * Adding docs
    
    * Tracking docs status

---
+++ docs/source/quickstart.rst
@@ -0,0 +1,46 @@
+**********
+Quickstart
+**********
+
+Install
+=======
+
+.. code-block:: bash
+
+    $ pip install rivulet
+
+
+A simple example
+================
+
+.. code-block:: python
+
+    # create a client
+    c = rivulet.connect(redis_url)
+
+    # subscribe 
+    channels = ['my-channel-0', 'my-channel-1']
+    c.subscribe(channels)
+
+    # write to a channel
+    c.write(channel_id, messages)
+
+    # read from a channel
+    inbox = c.read()
+    # inbox = {
+    #     "my-channel-1": [
+    #         {"id": 123,
+    #          "ts": 123465623,  # timestamp, ms since epoch
+    #          "src": "some-client-id",
+    #          "data": "the message as a string"}, ...],
+    #     "my-channel-2": ...
+    # }
+
+    # process all messages in all channels
+    for channel, messages in inbox:
+        for message in messages:
+            do_something(message['data'])
+
+    # unsusbscribe
+    c.unsubscribe(channels)
+
--- rivulet/rivulet.py
+++ rivulet/rivulet.py
@@ -1,4 +1,9 @@
-"""Redis-Based Message Streaming for Python."""
+"""
+**************************
+The :code:`rivulet` client
+**************************
+
+"""
 
 import enum
 import json
@@ -11,17 +16,18 @@ import redis
 import redis.exceptions
 from redis.client import Pipeline
 
-from rivulet.exceptions import ConnectionError, BackendError
+from rivulet.exceptions import ConnectionError, BackendError, TimeoutError
 
 
 class IndexPolicy(enum.Enum):
+    """Enum of rivulet index policies."""
     EARLIEST = 0
     CURRENT = 1
     LATEST = 2
 
 
 class Client:
-    """A Redis-based message streaming client."""
+    """A redis-based message broker."""
 
     def __init__(self,
                  redis_url: str,
@@ -29,24 +35,50 @@ class Client:
                  channel_ids: List[str] = None,
                  bufsize: int = 4096,
                  **redis_args) -> None:
-        self.redis = redis.from_url(
-            redis_url, encoding='utf-8', decode_responses=True, **redis_args)
+        try:
+            self.redis = redis.from_url(
+                redis_url,
+                encoding='utf-8',
+                decode_responses=True,
+                **redis_args)
+        except redis.exceptions.ConnectionError as e:
+            raise ConnectionError from e
+        except redis.exceptions.RedisError as e:
+            raise BackendError from e
         self.bufsize = bufsize
         self.client_id = client_id if client_id else uuid.uuid4().hex
         if channel_ids:
             self.subscribe(channel_ids)
 
     @property
-    def subscriptions(self):
+    def subscriptions(self) -> List[str]:
+        """
+        Returns the list of channels this client is subscribed to.
+
+        :return: A list of channel names.
+        :rtype: List[str]
+        :raises: BackendError, if anythong goes wrong with Redis.
+
+        .. note:: The implementation always actively queries the current list
+            of channel subscription from the Redis backend, the client does not
+            hold state and does not cache.
+
+        """
         try:
             subs = self.redis.zrange(f'indexes:client#{self.client_id}', 0, -1)
             return subs
         except redis.exceptions.RedisError as e:
             raise BackendError from e
 
-    def ping(self) -> None:
+    def ping(self) -> bool:
+        """
+        Ping the backend.
+
+        :returns: True if successful.
+
+        """
         try:
-            self.redis.ping()
+            return self.redis.ping()
         except redis.exceptions.RedisError as e:
             raise ConnectionError from e
 
@@ -54,7 +86,42 @@ class Client:
                   channel_ids: List[str],
                   index_policy: IndexPolicy = IndexPolicy.CURRENT,
                   timeout_ms: int = 1000) -> None:
+        """
+        Subscribe to a list of channels.
 
+        :param channel_ids: A list of channels to subscribe to.
+        :param index_policy: The index policy for channel subscription,
+                             one of :code:`EARLIEST`, :code:`CURRENT`,
+                             :code:`LATEST`.
+        :param timeout_ms: Time in milliseconds to wait for successful
+                           subscription. If subscription fails with a
+                           `TimeoutError`, the client is free to retry (use
+                           a backoff policy to avoid lock contention).
+        :raises: :code:`TimeoutError` if subscription fails withing the
+                 specified :code:`timeout_ms` interval. :code:`BackendError`
+                 for any other redis failures.
+
+
+        Index policies define how the client determines which message marks
+        the begin of a subscription:
+
+        :code:`IndexPolicy.EARLIEST`:
+            The subscriptions starts with the earliest message that is
+            still available in the channel.
+
+        :code:`IndexPolicy.CURRENT`:
+            The subscription starts with the current index of the client.
+            The current index is stored in the redis backend. This is a
+            helpful option to restart a subscription after the client was
+            restarted or similar. If there is no stored index for a
+            particular client & channel combination, `CURRENT` falls back to
+            `LATEST`.
+
+        :code:`IndexPolicy.LATEST`:
+            The subscription starts with the latest (i.e. most recent)
+            message in the channel.
+
+        """
         pipeline = self.redis.pipeline(transaction=True)
         for channel_id in channel_ids:
             try:
@@ -90,12 +157,27 @@ class Client:
                                   {channel_id: index})
                     pipeline.execute()
             except redis.exceptions.LockError as e:
-                raise BackendError from e
+                raise TimeoutError from e
             except redis.exceptions.RedisError as e:
                 raise BackendError from e
 
     def unsubscribe(self, channel_ids: List[str]) -> None:
+        """
+        Unsubscribes the client from a set of channels.
 
+        After unsubscribing the client, the function removes all
+        messages in the channel that have been read by all remaining
+        subscribers.
+        If there are no subscribers left the call will also delete
+        all remaining messages and control data structures from the
+        redis server.
+
+        :param channel_ids: A list of channels the unsubscribe from.
+                            Invalid or non-existent channels will *not* raise
+                            an error.
+        :raises: BackendError, if any redis command fails.
+
+        """
         try:
             pipeline = self.redis.pipeline(transaction=True)
             for channel_id in channel_ids:
@@ -123,6 +205,11 @@ class Client:
 
     def write(self, channel_id: str, data: str,
               timeout_ms: int = 10000) -> None:
+        """
+        Send a message to a channel/topic.
+
+        TBD
+        """
         try:
             with self.redis.lock(
                     f'lock:ids:channel#{channel_id}',
@@ -144,6 +231,11 @@ class Client:
 
     def read(self, timeout_ms: int = 0,
              message_limit: int = 512) -> Dict[str, List[str]]:
+        """
+        Read available messages from all channel subscriptions.
+
+        TBD
+        """
         try:
             current_indexes = self.redis.zrange(
                 f'indexes:client#{self.client_id}', 0, -1, withscores=True)
@@ -200,5 +292,19 @@ class Client:
 
 def connect(redis_url: str, client_id=None, channel_ids=None,
             **redis_args) -> Client:
+    """
+    Convenience function to create a rivulet client.
+
+    :redis_url: A conncetion URL for the redis backend, of the form
+                `https://[:password]@server:port/db`.
+    :client_id: A unique identifier for the client (default=uuid).
+    :channel_ids: A list of channel ids to connect to (optional).
+    :redis_args: Additional arguments to pass on to the redis `from_url()`
+                 constructor.
+    :return: A `rivulet` client object.
+    :raises:
+        `ConnectionError` if a connection to the redis server cannot be established.
+        `BackendError` if subscriptions cannot be carried out successfully.
+    """
     return Client(
         redis_url, client_id=client_id, channel_ids=channel_ids, **redis_args)
